5572bac94e719df2828ee7377f5e52f8c29a0eb1
MichaelBryantOU <mb281416@scc-mmlab-08.local>
2019-01-29 15:50:26 -0500

    Entire project uploaded

---
+++ Assets/Scripts/BluGuy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BluGuy : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform destination;
+    public float MoveSpeed = 5;
+    public float SlowSpeed = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(destination.position);
+        agent.speed = MoveSpeed;
+        agent.speed -= Time.time * SlowSpeed;
+    }
+}
---
+++ Assets/Scripts/RedGuy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RedGuy : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform destination;
+    public float MoveSpeed = 3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(destination.position);
+        agent.speed = MoveSpeed;
+    }
+}
